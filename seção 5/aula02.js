062d453e7b0504713da506303f3895dd9db7d55b
João Paulo da Silva Lima <joaopalima91@gmail.com>
2022-12-21 21:43:31 -0300

    criando arrays

---
+++ seção 5/aula02.js
@@ -0,0 +1,17 @@
+// forma 1
+var alunos = new Array('Cristina', 'Ricardo', 'Lucas', 'Maria', 'Sofia', 'João', 'Eva')
+console.log(alunos)
+alunos.sort() // ordena o array
+console.log(alunos) // imprime ordernado
+
+console.log('--------------------------------------------------')
+
+// forma 2 == RECOMENDADA
+let vet = [12, 24, 36, 48, 52]
+
+console.log(vet) // imprime o array
+
+console.log(vet[2]) // imprime na posção 2
+
+console.log('--------------------------------------------------')
+
