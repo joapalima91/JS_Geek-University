a640236c4d5d9101ffa49dab5fc54c5125e2c619
João Paulo da Silva Lima <joaopalima91@gmail.com>
2023-01-24 09:13:52 -0300

    aprendendo Funções Callbacks
    terminado aula 08

--- seção 6/aula08.js
+++ seção 6/aula08.js
@@ -29,3 +29,37 @@ cursos.forEach((curso, indice) => console.log(`${indice + 1} - ${curso}`));
 const precos = [23.55, 78.23, 899.43, 23.89, 5.23, 67.45];
 
 console.log(precos);
+
+let menores = []; // recebe os valores que 50
+/*
+for(let p in precos){
+    if(precos[p] < 50){
+        menores.push(precos[p])
+    }
+}
+console.log(menores);
+*/
+
+// criando for de maneira basica
+/*
+for (let i=0; i < precos.length; i++){
+    if(precos[i]<50){
+        menores.push(precos[i]);
+    }
+}
+console.log(menores);
+*/
+
+
+// Forma 1
+/*
+menores = precos.filter(function(preco){ //função anonima
+    return preco < 50;
+});
+
+console.log(menores);
+*/
+
+//Forma 2
+menores = precos.filter(preco => preco < 50); // filtra os valores de 'preços', executado na arrow, comparando preço
+console.log(menores);
