9152d3489f6cd44bf48def0224e8017bf2ca801a
João Paulo da SIlva lima <joaopalima91@gmail.com>
2023-01-23 21:55:52 -0300

    aula08
    aprendendo Funções Callbacks

---
+++ seção 6/aula08.js
@@ -0,0 +1,31 @@
+// aprendendo Funções Callbacks
+
+const cursos = [
+    'Programação para Leigos',
+    'Algoritmos e Lógica de Programação',
+    'Programação em C',
+    'Programação em Java',
+    'Programação em Python',
+    'Banco de Dados',
+    'Programação Web com Django Framework',
+    'Programação em JavaScript'
+]
+
+function apresentar(curso, indice){
+    console.log(`${indice + 1} - ${curso}`);
+}
+/*
+cursos.forEach(apresentar); // aqui está ocorrendo o callback
+
+// usando lambda
+cursos.forEach(function(curso, indice){
+    console.log(`${indice + 1} - ${curso}`);
+})
+
+// usando arrow
+cursos.forEach((curso, indice) => console.log(`${indice + 1} - ${curso}`));
+*/
+
+const precos = [23.55, 78.23, 899.43, 23.89, 5.23, 67.45];
+
+console.log(precos);
