de48fd6e3c94facd5e51ebd1d19805dc2584b644
João Paulo da Silva Lima <joaopalima91@gmail.com>
2023-01-30 17:21:37 -0300

    terminado aula 10
    funçoes Closures (contexto léxico de uma função)

--- seção 6/aula10.js
+++ seção 6/aula10.js
@@ -1,5 +1,5 @@
-// aprendendo funçoes Closures
-
+// aprendendo funçoes Closures (contexto léxico de uma função)
+/*
 let xuxa = 'Global'; // pode ser acessada globalmente no nosso projeto
 
 function imprimir(){
@@ -13,3 +13,19 @@ function outra(){
 }
 
 outra();
+*/
+
+let variavel = "Global";
+
+function externa(){
+    let variavel = 'Local';
+
+    function interna(){
+        return variavel;// retorno - local
+    }
+    return interna;
+}
+
+let executa = externa();
+
+console.log(executa()); // local
