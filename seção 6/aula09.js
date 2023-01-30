9b85a7ec37ea7bdb39b6b1d132a5de95f9d0be36
João Paulo da Silva Lima <joaopalima91@gmail.com>
2023-01-30 14:39:44 -0300

    terminado aula 06
    terminado tema sobre função construtora

--- seção 6/aula09.js
+++ seção 6/aula09.js
@@ -1,6 +1,6 @@
 // Aprendendo Funções Construtoras
 
-function Pessoa(n, s){
+function Pessoa(n, s, raca = 'Humano'){
     // atributos privados só conseguimos fazer uso dentro da função construtora
     let altura = 0.30;
     let peso = 0.5;
@@ -9,11 +9,11 @@ function Pessoa(n, s){
     let sexo = s;
     
     //atributo público pode ser acessado fora da função construtora
-    this.raca = 'Humano';
+    this.raca = raca;
 
     //método privado só conseguimos fazer uso dentro da função construtora
     let imprimirDados = function(){
-        console.log(`Nome: ${nome}, Idade: ${idade}, Peso: ${peso}, Altura: ${altura}, Sexp: ${sexo}`);
+        console.log(`Nome: ${nome}, Idade: ${idade}, Peso: ${peso}, Altura: ${altura}, Sexo: ${sexo}, Raça: ${raca}`);
     }
 
     // método público conseguimos acessar fora da função construtora
@@ -28,5 +28,24 @@ function Pessoa(n, s){
 
 }
 
+// Instanciando um objeto
+const joao = new Pessoa("João", "Masculino");
+console.log(joao)
+
+console.log(joao.peso); // privado
+
+console.log(joao.raca)
 
 
+joao.fazerAniversario(); // publico
+joao.fazerAniversario(); // publico
+
+// Instanciar um novo objeto
+
+const maria = new Pessoa("Maria", "Feminino");
+
+maria.fazerAniversario();
+console.log(maria.getIdade());
+
+console.log(typeof(Pessoa)); // aparece como function - ""Pessoa" é uma função construtora
+console.log(typeof(maria)); // object
