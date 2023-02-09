fe96a10431915a213a47476956b986219432723a
João Paulo da SIlva lima <joaopalima91@gmail.com>
2023-02-09 20:58:09 -0300

    terminado aula07

--- seção 7/aula07.js
+++ seção 7/aula07.js
@@ -5,8 +5,8 @@ const curso = {
     horas: 25,
     preco: 27.99
 }
-/*
 console.log(curso);
+/*
 console.log(typeof(curso)); // object
 console.log(Object.keys(curso)); // imprime as chaves do objeto do objeto no parâmetro [ 'nome', 'horas', 'preco' ]
 console.log(Object.values(curso)); // imprime os valores do objeto no parâmetro [ 'Programação em JavaScript', 25, 27.99 ]
@@ -25,3 +25,27 @@ Object.entries(curso).forEach(([chave, valor]) => {
 curso.nome = 'Criação de APIs Rest com Django Rest Framework'; // alterando nome do curso
 console.log(curso);
 */
+
+/*
+// alterando as propriedades do objeto
+Object.defineProperty(curso, 'publicacao', {
+    enumerable: false,
+    writable: false,
+    value: '09/02/2023'
+});
+
+
+curso.publicacao = '05/10/2023'; // não vai alterar pois atributo "writable" está "false"
+console.log(curso);
+console.log(curso.publicacao); //imprimndo atributo
+*/
+
+// arrar de objetos
+const usuarios = [
+    {nome: 'João Lima', empresa: 'Cognizant'},
+    {nome: 'Eva', empresa: 'BodyLaser'},
+    {nome: 'Daniela', empresa: 'Aker'}
+]
+console.log(usuarios);
+console.table(usuarios); // imprime em forma de tabela
+console.table(curso);
